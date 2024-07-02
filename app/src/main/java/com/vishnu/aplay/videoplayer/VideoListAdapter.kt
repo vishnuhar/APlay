0dcad74c1cc8d09b7206248c1af887f79b4c3dfd
VineeshHS <vineeshhs367@gmail.com>
2024-07-02 21:28:12 +0530

    added focus listener

--- app/src/main/java/com/vishnu/aplay/videoplayer/VideoListAdapter.kt
+++ app/src/main/java/com/vishnu/aplay/videoplayer/VideoListAdapter.kt
@@ -1,6 +1,8 @@
 package com.vishnu.aplay.videoplayer
 
 import android.content.Context
+import android.graphics.Color
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -39,9 +41,18 @@ class VideoListAdapter(private val context: Context, private val videoList: List
 
     override fun getItemCount(): Int = videoList.size
 
-    inner class VideoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class VideoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),View.OnFocusChangeListener, View.OnKeyListener {
         private val playerView: PlayerView = itemView.findViewById(R.id.player_view)
         private val thumbnailImage: ImageView = itemView.findViewById(R.id.thumbnail_image)
+
+        init {
+            itemView.isFocusable = true
+            itemView.isFocusableInTouchMode = true
+            itemView.onFocusChangeListener = this
+            itemView.setOnKeyListener(this)
+        }
+
+
 
         fun bind(videoItem: VideoItem) {
             Glide.with(context).load(videoItem.thumbnailUrl).into(thumbnailImage)
@@ -72,6 +83,46 @@ class VideoListAdapter(private val context: Context, private val videoList: List
                 exoPlayer.stop()
             }
         }
+
+        override fun onFocusChange(view: View?, hasFocus: Boolean) {
+            if (hasFocus) {
+
+
+                view?.let {
+                    it.scaleX = 1.1f
+                    it.scaleY = 1.1f
+                }
+            } else {
+                view?.let {
+                    it.scaleX = 1.0f
+                    it.scaleY = 1.0f
+                }
+            }
+        }
+
+        override fun onKey(view: View?, keyCode: Int, event: KeyEvent?): Boolean {
+            if (event?.action == KeyEvent.ACTION_DOWN) {
+                when (keyCode) {
+                    KeyEvent.KEYCODE_DPAD_CENTER, KeyEvent.KEYCODE_ENTER -> {
+                        playSelectedVideo(bindingAdapterPosition)
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+    }
+
+    private fun playSelectedVideo(position: Int) {
+        playbackJob?.cancel()
+        currentPlayerIndex = position
+        notifyItemChanged(currentPlayerIndex)
+        val videoItem = videoList[position]
+        val exoPlayer = exoPlayers[position]
+        val mediaItem = MediaItem.fromUri(videoItem.videoUrl)
+        exoPlayer?.setMediaItem(mediaItem)
+        exoPlayer?.prepare()
+        exoPlayer?.playWhenReady = true
     }
 
     private fun playNextVideo() {
